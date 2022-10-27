ffa3b4999ab523088ef4bf95e3deba52c7e04fc7
lizongrong <lizongrong@meican.com>
2022-10-27 17:03:09 +0800

    lesson7: mybatis

---
+++ mybatis/README.md
@@ -0,0 +1,11 @@
+使用MyBatis框架操作数据，在SpringBoot框架集成MyBatis 
+使用步骤：  
+1.mybatisi起步依赖：完成mybatis对象自动配置，对象放在容器中  
+2.pom.xml指定把src/main/java目录中的xml文件包含到classpath中  
+3.创建实体类Student  
+4.创建Dao接口StudentDao,创建一个查询学生的方法  
+5.创建Dao接口对应的Mapper文件，xml文件，写sql语句  
+6.创建Service层对象，创建StudentService接口和他的实现类。去dao对象的方法。完成数据库的操作  
+7.创建Controller对象，访问Service。  
+8.写application.properties.文件  
+配置数据库的连接信总。  
---
+++ mybatis/src/main/java/com/example/mybatis/model/Student.java
@@ -0,0 +1,42 @@
+package com.example.mybatis.model;
+
+public class Student {
+	private Integer id;
+	private String name;
+	private Integer age;
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return this.age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "{" +
+			" id='" + getId() + "'" +
+			", name='" + getName() + "'" +
+			", age='" + getAge() + "'" +
+			"}";
+	}
+	
+	
+}
